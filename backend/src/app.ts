9a89be722bd3dd24af53a89f258e74f1943e3b74
Scrub <scott-donaldson@outlook.com>
2023-07-12 21:56:34 +0100

    added web specific packages

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,34 @@
 import chalk from "chalk"
-import { figletText, print, printBool, printDevOptions } from "./utility/print.js"
-import config from "./utility/config.js"
-import { TDevOptions } from "./utility/types.js"
-const run = () => {
+import { debugPrint, figletText, iniPrint, print, printDevOptions } from "./utility/print.js"
+import config, { DEV_OPTIONS } from "./utility/config.js"
+import { deleteDatabase, doesDBExist, initialise } from "./database/initialise.js"
+import { log } from "console"
+
+const printWelcome = () => {
   print(figletText(config.NAME) +" v"+ chalk.underline(config.VERSION))
+  print(chalk.bold.underline(config.DESC)+ "\n")
+
+  print(chalk.bold(`Port: ${config.PORT}`))
   printDevOptions()
+  print("\n")
+}
+
+const initaliseDatabase = () => {
+  if(doesDBExist() && !DEV_OPTIONS.PERSIST_DB) {
+    debugPrint("Removing previous database")
+    deleteDatabase()
+  }
+  else {
+    iniPrint("Initalising database")
+    initaliseDatabase()
+  }
+}
+
+
+
+const run = () => {
+  printWelcome()
+  initialise()
 }
 
 run()
